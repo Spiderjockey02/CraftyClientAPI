d3844be358a5c4ab6f550c1f17c7f1df4941e060
Spiderjockey02 <benjamin.forey11@gmail.com>
2021-12-15 12:32:01 +0000

    Fixed runCommand endpoint

--- src/functions.js
+++ src/functions.js
@@ -1,16 +1,16 @@
-const axios = require('axios'),
-	https = require('https');
+const axios = require('axios');
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 
 module.exports = {
 	sendPostRequest: async (url, data) => {
-		console.log(data);
 		try {
-			const resp = await axios.post(url, {
-				httpsAgent: new https.Agent({
-					rejectUnauthorized: false,
-				}),
-				command: data.command,
+			const resp = await axios({
+				method: 'POST',
+				url: url,
+				headers: {
+					'Content-Type': 'application/x-www-form-urlencoded',
+				},
+				data: `command=${data.command}`,
 			});
 			console.log(resp);
 		} catch (e) {
@@ -21,12 +21,9 @@ module.exports = {
 	sendGetRequest: async (url) => {
 		try {
 			// Send request
-			const resp = await axios.get(url, {
-				httpsAgent: new https.Agent({
-					rejectUnauthorized: false,
-				}),
-			});
+			const resp = await axios.get(url);
 
+			// Check request
 			if (resp.status == '200') {
 				return resp.data;
 			} else {
