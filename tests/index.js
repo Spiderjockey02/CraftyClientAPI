7975ce0e855297f81e28d32b12a683e41e072185
Spiderjockey02 <benjamin.forey11@gmail.com>
2021-12-27 00:02:59 +0000

    Updated to version 1.0.1

--- tests/index.js
+++ tests/index.js
@@ -2,6 +2,8 @@ const CraftyAPI = require('../src/index.js');
 
 const API = new CraftyAPI('https://192.168.0.1:8000', 'AdminKeyYouWontGet');
 
-console.log('Fetching hosts');
-API.fetchHost();
-API.runCommand(4, 'save-all');
+(async () => {
+	console.log('Fetching hosts');
+	console.log(await API.fetchHost());
+	API.runCommand(4, 'save-all');
+})();
